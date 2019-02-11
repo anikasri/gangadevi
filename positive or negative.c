e3d2abbf87cb504a77409ce0d3a36fce1551b9cb
anikasri <43335325+anikasri@users.noreply.github.com>
2019-02-11 10:28:46 +0530

    Create positive or negative.c

---
+++ positive or negative.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+int c;
+scanf("%d",c);
+if(c<0);
+{
+printf("positive");
+}
+elseif(c>o);
+{
+printf("negative");
+}
+printf("zero");
+getch();
+}
