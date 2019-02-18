e3aa2786057d1add9032976fb188ba4e2ce359cd
anikasri <43335325+anikasri@users.noreply.github.com>
2019-02-18 10:32:47 +0530

    Create prime or not.c

---
+++ prime or not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+int a,n,count=0;
+scanf("%d",&a);
+for(n=2;n<a;n++)
+{
+if(a%n==0)
+count++;
+}
+if(count==0)
+printf("prime number\n");
+else
+printf("not prime number\n");
+}
